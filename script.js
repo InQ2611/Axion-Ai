24485a2f12f62945585d838ba9a7b4ffd9aae0be
Xqin <iinghoniyyah@gmail.com>
2026-07-03 13:50:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -37,3 +37,41 @@ document.getElementById("dashboard").style.display="block";
 }
 
 },800);
+
+function clock(){
+
+const d=new Date();
+
+document.getElementById("clock").innerHTML=
+
+d.toLocaleTimeString();
+
+}
+
+setInterval(clock,1000);
+
+clock();
+
+let u=0;
+
+let g=0;
+
+setInterval(()=>{
+
+if(u<15284){
+
+u+=93;
+
+document.getElementById("users").innerHTML=u.toLocaleString();
+
+}
+
+if(g<2481){
+
+g+=15;
+
+document.getElementById("groups").innerHTML=g.toLocaleString();
+
+}
+
+},20);
